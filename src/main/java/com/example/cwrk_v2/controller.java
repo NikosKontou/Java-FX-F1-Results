ec8ed903a3fddba3ff9407c8fb8ae1d2f9458da4
twelveYearsOldCoder <niko.ss@windowslive.com>
2022-03-15 21:32:26 +0200

    nothing added or changed

--- src/main/java/com/example/cwrk_v2/controller.java
+++ src/main/java/com/example/cwrk_v2/controller.java
@@ -38,10 +38,6 @@ public class controller {
     }
 
     public void logoutFromMainMenu( ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("loginScene.fxml"));
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+
     }
 }
--- src/main/java/com/example/cwrk_v2/startingSceneController.java
+++ src/main/java/com/example/cwrk_v2/startingSceneController.java
@@ -1,17 +1,40 @@
 package com.example.cwrk_v2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class startingSceneController {
     @FXML
     Label nameLabel;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     public void displayName (String username){
-        nameLabel.setText("Hello "+username);
+        if (!username.isEmpty()) {
+            nameLabel.setText(username);
+        }
     }
-    public void goToMainMenuEvent(){
+    public void goToMainMenuEvent(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("loginScene.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
+    }
+    public void showDataEvent(){
+
+    }
+    public void insertDataEvent(ActionEvent event) throws IOException {
 
     }
 
